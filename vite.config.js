f3d3061de75339d045bd235895a0ed6f5312ff8d
Shcoobz <a.boeckmann23@gmail.com>
2024-05-21 12:50:11 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/react_bookmark-keeper/',
+  base: '/advancedJS_bookmark-keeper/',
   build: {
     outDir: 'build',
   },
